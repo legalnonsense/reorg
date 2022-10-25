7ec2e7c6f6e67f325fc5c425a880e5e90d2367b5
legalnonsense <jrfilipovits@gmail.com>
2022-10-25 10:52:03 -0400

    start figuring out new insertion method

--- reorg-scratch.el
+++ reorg-scratch.el
@@ -11,45 +11,23 @@
 		     (reorg--group-and-sort it template))))
   results)
 
+;; is the current data a branch or leaf?
+;; if it is a heading, is the heading present?
+;; Go to it. loop to next data
+;; if it is a leaf, insert it at the appropriate point
 
 
-(defun reorg--turn-at-dot-to-dot (elem)
-  "turn a .@symbol into .symbol
-the value from the parsed data or calling the display
-function created by the type creation macro."
-  (if (and (symbolp elem)
-	   (string-match "\\`\\.@" (symbol-name elem)))
-      (intern (concat "." (substring (symbol-name elem) 2)))
-    elem))
+;; heading present?
+;; subheading present?
+;; find header location 
 
-(defun reorg--depth-first-apply (form func &optional data)
-  "Run FUNC at each node of TREE using a depth-first traversal
-and destructively modify TREE. 
-FUNC is a function that accepts one argument, which is the
-current element of TREE."
-  (let ((tree (copy-tree form)))
-    (cl-labels ((doloop (tree func)
-			(setf (car tree) (funcall func (car tree)))
-			(cl-loop for n below (length (cdr tree))
-				 if (listp (nth n (cdr tree))) do
-				 (doloop (nth n (cdr tree)) func)
-				 else do
-				 (setf (nth n (cdr tree))
-				       (funcall func (nth n (cdr tree)))))))
-      (if (listp tree)
-	  (progn 
-	    (doloop tree func)
-	    tree)
-	(funcall func tree)))))
+(md5 (with-temp-buffer
+       (insert
+	(pp '(a b)))
+
+       "sadfasdaa")
+     (buffer-substring-no-properties (point-min) (point-max))))
 
 
 
-(cl-flet ((xxx (a) (+ 1 a)))
-  (reorg--depth-first-apply '(1 2 (3 4)) #'xxx))
 
-(let ((yyy '(concat " " .@a)))
-  (reorg--depth-first-apply yyy #'reorg--turn-at-dot-to-dot))
-
-
-
-xxx
