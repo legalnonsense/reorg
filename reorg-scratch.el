bbcc6dcfb1d39554fc9da1f953037e3c1098b120
Jeff Filipovits <jrfiliplovits@gmail.com>
2022-10-24 23:41:26 -0400

    fixed depth first apply

--- reorg-scratch.el
+++ reorg-scratch.el
@@ -1,42 +1,55 @@
 ;; -*- lexical-binding: t; -*-
 
-
-(defun reorg--dot-at-search (data)
-  "Return alist of symbols inside DATA that start with a `.'.
-Perform a deep search and return an alist where each car is the
-symbol, and each cdr is the same symbol without the `.'."
-  (cond
-   ((symbolp data)
-    (let ((name (symbol-name data)))
-      (when (string-match "\\`\\.@" name)
-        ;; Return the cons cell inside a list, so it can be appended
-        ;; with other results in the clause below.
-        (list (cons data (intern (replace-match "" nil nil name)))))))
-   ((vectorp data)
-    (apply #'nconc (mapcar #'reorg--dot-at-search data)))
-   ((not (consp data)) nil)
-   ((eq (car data) 'let-alist)
-    ;; For nested ‘let-alist’ forms, ignore symbols appearing in the
-    ;; inner body because they don’t refer to the alist currently
-    ;; being processed.  See Bug#24641.
-    (reorg--dot-at-search (cadr data)))
-   (t (append (reorg--dot-at-search (car data))
-              (reorg--dot-at-search (cdr data))))))
-
-(setq
- yyy
- (let ((grouper (lambda (x) (let-alist x (when .at-name .@tag-list)))))
-   (cl-loop for each in xxx
-	    if (listp (funcall grouper each))
-	    append (cl-loop
-		    with dots = (cl-delete-duplicates
-				 (let-alist--deep-dot-search grouper)
-				 :test #'equal)
-		    for (_ . dot) in dots
-		    append (cl-loop for x in (alist-get dot each)
-				    collect (let ((ppp (copy-alist each)))
-					      (setf (alist-get dot ppp) x)
-					      ppp)))
-	    else append each)))
+;; 1.
 
 
+(let* ((template '( :group .@tag-list
+		    :format-string (concat " " .headline)
+		    :children (( :group .@at-name
+				 :format-string (concat " " .headline)))))
+       (results (--> (reorg--getter '((org . "~/.emacs.d/lisp/reorg/TESTS/new.org")))
+		     (reorg--group-and-sort it template))))
+  results)
+
+
+
+(defun reorg--turn-at-dot-to-dot (elem)
+  "turn a .@symbol into .symbol
+the value from the parsed data or calling the display
+function created by the type creation macro."
+  (if (and (symbolp elem)
+	   (string-match "\\`\\.@" (symbol-name elem)))
+      (intern (concat "." (substring (symbol-name elem) 2)))
+    elem))
+
+(defun reorg--depth-first-apply (form func &optional data)
+  "Run FUNC at each node of TREE using a depth-first traversal
+and destructively modify TREE. 
+FUNC is a function that accepts one argument, which is the
+current element of TREE."
+  (let ((tree (copy-tree form)))
+    (cl-labels ((doloop (tree func)
+			(setf (car tree) (funcall func (car tree)))
+			(cl-loop for n below (length (cdr tree))
+				 if (listp (nth n (cdr tree))) do
+				 (doloop (nth n (cdr tree)) func)
+				 else do
+				 (setf (nth n (cdr tree))
+				       (funcall func (nth n (cdr tree)))))))
+      (if (listp tree)
+	  (progn 
+	    (doloop tree func)
+	    tree)
+	(funcall func tree)))))
+
+
+
+(cl-flet ((xxx (a) (+ 1 a)))
+  (reorg--depth-first-apply '(1 2 (3 4)) #'xxx))
+
+(let ((yyy '(concat " " .@a)))
+  (reorg--depth-first-apply yyy #'reorg--turn-at-dot-to-dot))
+
+
+
+xxx
