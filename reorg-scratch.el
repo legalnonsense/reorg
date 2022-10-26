e0c87f6da134ba26165d024454f58dde61b951d7
legalnonsense <jrfilipovits@gmail.com>
2022-10-26 09:20:51 -0400

    scratch

--- reorg-scratch.el
+++ reorg-scratch.el
@@ -11,22 +11,50 @@
 		     (reorg--group-and-sort it template))))
   results)
 
-;; is the current data a branch or leaf?
-;; if it is a heading, is the heading present?
-;; Go to it. loop to next data
-;; if it is a leaf, insert it at the appropriate point
+(defun reorg-insert--find-first-branch (level &optional backward)
+  "find the first branch that matches LEVEL."
+  (let* ((func (if backward
+		   #'text-property-search-backward
+		 #'text-property-search-forward))
+	 (match (funcall func 
+			 'reorg-field-type
+			 'branch))
+	 (level (reorg--get-view-props nil 'reorg-data 'reorg-level)))
+    level))
 
 
-;; heading present?
-;; subheading present?
-;; find header location 
+      (insert xxx)
+      * ccc
 
-(md5 (with-temp-buffer
-       (insert
-	(pp '(a b)))
 
-       "sadfasdaa")
-     (buffer-substring-no-properties (point-min) (point-max))))
+      1234567
+
+
+      (put-text-property 580 585 'reorg-field-type 'branch)
+
+      (lambda (val alist)
+	(string=
+	 (alist-get 'id alist)
+	 val))
+      'not-current))
+  (point (point)))
+(
+
+ ;; is the current data a branch or leaf?
+ ;; if it is a heading, is the heading present?
+ ;; Go to it. loop to next data
+ ;; if it is a leaf, insert it at the appropriate point
+
+
+ ;; heading present?
+ ;; subheading present?
+ ;; find header location 
+
+ (md5 (with-temp-buffer
+	(insert
+	 (pp '(a b)))
+	(buffer-string)))
+
 
 
 
