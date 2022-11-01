7d7fb190dfdbc80daffb5880f997e13c8638bc09
Jeff Filipovits <jrfiliplovits@gmail.com>
2022-10-31 20:22:25 -0400

    remove .stars

--- reorg.el
+++ reorg.el
@@ -35,7 +35,7 @@
   "Which side for the tree buffer?")
 (defcustom reorg-face-text-prop 'font-lock-face
   "When setting a face, use this text property.")
-(defcustom reorg-headline-format '(concat .stars " " .headline)
+(defcustom reorg-headline-format '(concat " " .headline)
   "Default headline format.")
 
 ;;; variables 
