8f4b9c2133d84ef8c6ac86da37336a551fdc4afd
Jeff Filipovits <jrfiliplovits@gmail.com>
2022-11-08 09:39:16 -0500

    Refactor candidates to separate function

--- reorg-devel.el
+++ reorg-devel.el
@@ -1,6 +1,28 @@
 ;; -*- lexical-binding: t; -*-
 
 
+(defun reorg-company--get-candidates (arg)
+  "Get company candidates."
+  (let* ((all-dots (sort
+		    (delete-dups 
+		     (cl-loop for (class . rest) in reorg--parser-list
+			      append 
+			      (cl-loop for (type . func)
+				       in rest
+				       collect
+				       (concat "." (symbol-name type)))))
+		    #'string<)))
+    (if (equal arg "..")
+	all-dots
+      (let ((target (and (string-match "\\(^\.\.?\\)\\(.+\\)" arg)
+			 (match-string 2 arg))))
+	(if-let ((class (alist-get (intern target) reorg--parser-list)))
+	    (sort (cl-loop for (type . func) in class
+			   collect (concat "." (symbol-name type))) #'string<) 
+	  (let ((root (concat "." (match-string 2 arg))))
+	    (cl-remove-if-not (lambda (x) (s-starts-with-p root x))
+			      all-dots)))))))
+
 (defun reorg-company (command &optional arg &rest ignored)
   "company backend"
   (cl-case command
@@ -11,25 +33,15 @@
 	    (when (equal "."
 			 (substring sym 0 1))
 	      sym))))
-    (candidates (let* ((all-dots (sort
-				  (delete-dups 
-				   (cl-loop for (class . rest) in reorg--parser-list
-					    append 
-					    (cl-loop for (type . func)
-						     in rest
-						     collect
-						     (concat "." (symbol-name type)))))
-				  #'string<)))
-		  (if (equal arg "..")
-		      all-dots
-		    (string-match "\\(^\.\.?\\)\\(.+?\\)" arg)
-		    (let ((root (concat "." (match-string 2 arg))))
-		      (cl-remove-if-not (lambda (x) (s-starts-with-p root x))
-					all-dots)))))
+    (candidates (reorg-company--get-candidates arg))
     (sorted t)
     (no-cache t)))
 
 (add-to-list 'company-backends #'reorg-company)
 
-
 (provide 'reorg-devel)
+
+
+
+
+
