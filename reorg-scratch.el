d794ed6489bf657049317e24ee96ee28043f62ac
Jeff Filipovits <jrfiliplovits@gmail.com>
2022-11-10 21:06:36 -0500

    wip

--- reorg-scratch.el
+++ reorg-scratch.el
@@ -1,88 +1,66 @@
 ;; -*- lexical-binding: t; -*-
 
-;; 1.
-
-
-(let* ((template '( :group .@tag-list
-		    :format-string (concat " " .headline)
-		    :children (( :group .@at-name
-				 :format-string (concat " " .headline)))))
-       (results (--> (reorg--getter '((org . "~/.emacs.d/lisp/reorg/TESTS/new.org")))
-		     (reorg--group-and-sort it template))))
-  results)
-
-(defun reorg-insert--find-by-prop (prop val &optional test backward)
-  "find the first branch that matches LEVEL."
-  (when-let* ((func (if backward
-			#'text-property-search-backward
-		      #'text-property-search-forward))
-	      (match (funcall func 
-			      'reorg-data
-			      val
-			      (lambda (a b) (funcall
-					     (or test #'string=)
-					     (alist-get prop b)
-					     a))
-			      t)))
-    (goto-char (prop-match-beginning match))))
-
-(defun reorg--find-prop (prop &optional val from to test)
-  (cl-flet ((truth (&rest args) t))
-    (cl-loop with test = (if val
-			     (or test #'equal )
-			   #'truth)
-	     for (beg . end) being the intervals
-	     property 'reorg-data
-	     from (or from (point-min))
-	     to (or to (point-max))
-	     when (funcall test
-			   (alist-get prop
-				      (get-text-property beg 'reorg-data))
-			   val)
-	     collect (cons beg end))))
-
-(defun reorg--truth (&rest _args)
-  "Ignore everything and return t."
-  t)
-
-(defun reorg--and (a b)
-  "Ignore everything and return t."
-  (and a b))
-
-(reorg--find-prop 'reorg-data t
-		  'reorg--and
-		  nil nil
-		  (lambda (x)
-		    (get-text-property x 'reorg-data)
-		    ))
 
 
 
 
+(defun xxx-create-test-data ()
+  (interactive)
+  (cl-loop for x below 10
+	   collect (cl-loop for b in '(a b c d e f g h i j k l m n o p)
+			    collect (cons b (random 100)))))
+
+(setq xxx-data (xxx-create-test-data))
+(setq xxx-template
+      '( :children (( :group (lambda (x) (when (oddp (alist-get 'a x))
+					   (concat "A: "
+						   (number-to-string 
+						    (alist-get 'a x)))))
+		      :sort-group (lambda (a b)
+				    (string>
+				     (car a)
+				     (car b)))
+		      :children (( :group (lambda (x) (/= 66 (alist-get 'b x))
+					    "EQUALS 66"))))
+		    
+		    ( :group (lambda (x) (when (evenp (alist-get 'b x))
+					   (concat "B: "
+						   (number-to-string
+						    (alist-get 'b x)))))))))
+
+(defun xxx (data template)
+  (cl-loop with results = nil
+	   for groups in (plist-get template :children)
+	   collect (let* ((result (reorg--seq-group-by* (plist-get groups :group)
+							data))
+			  (result (if (plist-get groups :sort-group)
+				      (seq-sort (plist-get groups :sort-group)
+						result)
+				    result))
+			  (result (if (plist-get groups :children)
+				      (cl-loop for e in result
+					       collect (xxx (cadr e) groups))
+				    result)))
+		     result)))
 
 
 
+(cl-defgeneric reorg--seq-group-by* (func sequence)
+  "Apply FUNCTION to each element of SEQUENCE.
+Separate the elements of SEQUENCE into an alist using the results as
+keys.  Keys are compared using `equal'.  Do not group results
+that return nil."
+  (seq-reduce
+   (lambda (acc elt)
+     (let* ((key (funcall func elt))
+	    (cell (assoc key acc)))
+       (if cell
+	   (setcdr cell (push elt (cdr cell)))
+	 (when key
+	   (push (list key elt) acc)))
+       acc))
+   (seq-reverse sequence)
+   nil))
 
-(insert xxx)
-* ccc
+(xxx xxx-data xxx-template) ;;;test  
 
-;; is the current data a branch or leaf?
-;; if it is a heading, is the heading present?
-;; Go to it. loop to next data
-;; if it is a leaf, insert it at the appropriate point
-
-
-;; heading present?
-;; subheading present?
-;; find header location 
-
-(md5 (with-temp-buffer
-       (insert
-	(pp '(a b)))
-       (buffer-string)))
-
-
-
-
-(cl-loop for (a . b) being the intervals
-	 collect a)
