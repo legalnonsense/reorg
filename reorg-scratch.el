ceb888c163e61307d63e79044b0b1b36268ce8df
legalnonsense <jrfilipovits@gmail.com>
2022-11-14 21:11:21 -0500

    wip

--- reorg-scratch.el
+++ reorg-scratch.el
@@ -1,9 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 
-
-
-
-
 (defun xxx-create-test-data ()
   (interactive)
   (cl-loop for x below 1000
@@ -11,7 +7,7 @@
 			    '(a b c d)
 			    collect (cons b (random 10)))))
 
-(setq xxx-data (xxx-create-test-data)) 
+(setq xxx-data (xxx-create-test-data))
 (setq xxx-template
       '( :children (( :group (lambda (x) (when (oddp (alist-get 'a x))
 				 	   (concat "A: "
@@ -31,69 +27,46 @@
 		      :sort-results (
 				     ((lambda (x) (alist-get 'b x)) . >))))))
 
-
-(reorg--multi-sort '(((lambda (a) (alist-get 'd a)) . <)) xxx-data)
-
-;; ( :group (lambda (x) (when (evenp (alist-get 'b x))
-;; 			     (concat "B: "
-;; 				     (number-to-string
-;; 				      (alist-get 'b x)))))))))
-
-;; (defun xxx (data template &optional sorters)
-;;   (cl-loop with results = nil
-;; 	   with sorters = (append sorters (plist-get template :result-sorters))
-;; 	   for groups in (plist-get template :children)
-;; 	   append (let* ((result (reorg--seq-group-by* (plist-get groups :group)
-;; 						       data))
-;; 			 (result (if (plist-get groups :sort-group)
-;; 				     (seq-sort (plist-get groups :sort-group)
-;; 					       result)
-;; 				   result))
-;; 			 (result (cond ((plist-get groups :children)
-;; 					(cl-loop for e in result
-;; 						 collect (cons (car e) (xxx (cdr e) groups))))
-;; 				       (sorters
-;; 					(reorg--multi-sort sorters result))
-;; 				       (t result))))
-;; 		    result)))
-
-;; (defun reorg--multi-sort* (functions sequence &optional test)
-;;   "FUNCTIONS-AND-PREDICATES is an alist of functions and predicates.
-;; It uses the FUNCTION and PREDICATE arguments useable by `seq-sort-by'.
-;; SEQUENCE is a sequence to sort."
-;;   (seq-sort 
-;;    (lambda (a b)
-;;      (cl-loop for func in functions
-;; 	      unless (funcall (or test #'equal) a b)
-;; 	      return (funcall func a b)))
-;;    sequence))
-
-(defun xxx-thread-as-rewrite (data template &optional sorters)
+(defun xxx-thread-as-rewrite (data
+			      template
+			      &optional
+			      sorters
+			      format-string
+			      level)
   (cl-loop 
    for groups in (plist-get template :children)
-   do (setq sorters (append sorters (plist-get groups :sort-results)))
-   append (-as-> data data
-		 (reorg--seq-group-by* (plist-get groups :group)
-				       data)
-		 (if (plist-get groups :sort-group)
-		     (seq-sort (plist-get groups :sort-group)
-			       data)
-		   data)  
-		 (cond ((plist-get groups :children)
-			(cl-loop for e in data
-				 collect
-				 (cons (car e)
-				       (xxx-thread-as-rewrite
-					(cdr e)
-					groups
-					sorters))))
-		       (sorters
-			(cl-loop for each in data
-				 collect
-				 (cons (car each)
-				       (reorg--multi-sort sorters (cdr each)))))
-		       (t
-			data)))))
+   do (setq format-string (or format-string
+			      (plist-get template :format-string)
+			      reorg-headline-format))
+   and do (setq sorters (append sorters (plist-get groups :sort-results)))
+   and do (setq level (or level 1))
+   append (reorg--thread-as data
+	    (reorg--seq-group-by* (plist-get groups :group)
+				  data)
+	    (if (plist-get groups :sort-group)
+		(seq-sort (plist-get groups :sort-group)
+			  data)
+	      data)  
+	    (cond ((plist-get groups :children)
+		   (cl-loop for e in data
+			    collect
+			    (cons (car e)
+				  (xxx-thread-as-rewrite
+				   (cdr e)
+				   groups
+				   sorters
+				   format-string
+				   (1+ level)))))
+		  (sorters ;; no more headers
+		   (cl-loop for each in data
+			    collect
+			    (cons (car each)
+				  (reorg--multi-sort sorters (cdr each)))))
+		  (t ;; no more headers; no sort
+		   (cl-loop for (header . rest) in data
+			    collect (cons header rest)))))))
+
+
 
 (defmacro reorg--thread-as (name &rest form)
   "like `-as->' but better!?"
@@ -128,8 +101,7 @@ that return nil."
    (seq-reverse sequence)
    nil))
 
-(equal (xxx xxx-data xxx-template) 
-       (xxx-thread-as-rewrite xxx-data xxx-template))
 
-(xxx-thread-as-rewrite xxx-data xxx-template) ;;;test
 
+
+(xxx-thread-as-rewrite xxx-data xxx-template) ;;;test 
