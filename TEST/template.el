f145dac2312fcf3ea788d3fc75b7051611490ac3
Jeff Filipovits <jrfiliplovits@gmail.com>
2023-01-11 17:18:19 -0500

    wip

--- TEST/template.el
+++ TEST/template.el
@@ -25,32 +25,32 @@
 			 :format-results (.priority "\t" (upcase .headline))
 			 :sort-results ((.tags . string<)))))
 	  ( :group "All headings, but only IDs displayed"
-	    :format-results (.id))
-	  ( :sources ((elisp . "../reorg.el"))
-	    :group (when (member .form-type '("defvar" "defcustom" "defconst"))
-		     (concat 
-		      "All functions in the reorg source code, "
-		      "with the reorg- prefix removed, sorted alpabetically, "
-		      "and grouped by type, and displayed with a large indent"))
-	    :children (( :group .form-type 
-			 :format-results ((propertize " " 'display '(space . (:align-to 30)))
-					  (replace-regexp-in-string "reorg-?-"
-								    ""
-								    .form-name))
-			 :sort-results ((.form-name . string<)))))
-	  ( :sources ((files . "find ~/.emacs.d/lisp/reorg/ -type f | grep .el$"))
-	    :group (when (string= .extension "el")
-		     (concat 
-		      "All reorg elisp files, grouped by length of file name, "
-		      "with smiles next to filenames containing an A"))
-	    :children (( :group (number-to-string (length .filename))
-			 :sort-results string>
-			 :sort-groups (lambda (a b) (< (string-to-number a)
-						       (string-to-number b)))
-			 :format-results ((concat
-					   (when (s-contains-p "a" .filename)
-					     ":-) ")
-					   .filename))))))))
+	    :format-results (.id)))))
+	  ;; ( :sources ((elisp . "../reorg.el"))
+	  ;;   :group (when (member .form-type '("defvar" "defcustom" "defconst"))
+	  ;; 	     (concat 
+	  ;; 	      "All functions in the reorg source code, "
+	  ;; 	      "with the reorg- prefix removed, sorted alpabetically, "
+	  ;; 	      "and grouped by type, and displayed with a large indent"))
+	  ;;   :children (( :group .form-type 
+	  ;; 		 :format-results ((propertize " " 'display '(space . (:align-to 30)))
+	  ;; 				  (replace-regexp-in-string "reorg-?-"
+	  ;; 							    ""
+	  ;; 							    .form-name))
+	  ;; 		 :sort-results ((.form-name . string<)))))
+	  ;; ( :sources ((files . "find ~/.emacs.d/lisp/reorg/ -type f | grep .el$"))
+	  ;;   :group (when (string= .extension "el")
+	  ;; 	     (concat 
+	  ;; 	      "All reorg elisp files, grouped by length of file name, "
+	  ;; 	      "with smiles next to filenames containing an A"))
+	  ;;   :children (( :group (number-to-string (length .filename))
+	  ;; 		 :sort-results string>
+	  ;; 		 :sort-groups (lambda (a b) (< (string-to-number a)
+	  ;; 					       (string-to-number b)))
+	  ;; 		 :format-results ((concat
+	  ;; 				   (when (s-contains-p "a" .filename)
+	  ;; 				     ":-) ")
+	  ;; 				   .filename))))))))
 
 
 (reorg-open-in-current-window reorg-test-template)
