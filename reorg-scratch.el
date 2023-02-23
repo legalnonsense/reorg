2fa0ef7cdc1b178c27a32363a43ab6398eb4b655
Jeff Filipovits <jrfiliplovits@gmail.com>
2023-02-23 07:52:11 -0500

    wip

--- reorg-scratch.el
+++ reorg-scratch.el
@@ -1,5 +1,99 @@
 ;; -*- lexical-binding: t; -*-
 
+;; This buffer is for text that is not saved, and for Lisp evaluation.
+;; To create a file, visit it with C-x C-f and enter text in its buffer.
+
+'((( :headline "* By client\n"
+     :id "b92c491098343a3ee048af2ae7d5eebaBy client"
+     :group-id "b92c491098343a3ee048af2ae7d5eeba"
+     :parent-id nil)
+   ( :headline "** Abraha, Seifu\n"
+     :id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu"
+     :group-id "8c7bd4ee270a7b45899af20b5cb0f702"
+     :parent-id "b92c491098343a3ee048af2ae7d5eebaBy client")
+   ( :headline "*** Tasks\n"
+     :id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu4297c1f66ff67d8df8962821c7d8d81eTasks"
+     :group-id "4297c1f66ff67d8df8962821c7d8d81e"
+     :parent-id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu")
+   ( :headline "⚡ TASK            this is a test!\n"
+     :id "d51e0c82-2005-4e7a-90d9-eb3024c4276a"
+     :group-id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu4297c1f66ff67d8df8962821c7d8d81eTasks"
+     :parent-id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu4297c1f66ff67d8df8962821c7d8d81eTasks"))
+  (( :headline "* By client\n"
+     :id "b92c491098343a3ee048af2ae7d5eebaBy client"
+     :group-id "b92c491098343a3ee048af2ae7d5eeba"
+     :parent-id nil)
+   ( :headline "** Abraha, Seifu\n"
+     :id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu"
+     :group-id "8c7bd4ee270a7b45899af20b5cb0f702"
+     :parent-id "b92c491098343a3ee048af2ae7d5eebaBy client")
+   ( :headline "*** Calendar\n"
+     :id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu235fe0000369a4c2d171b16de8008479Calendar"
+     :group-id "235fe0000369a4c2d171b16de8008479"
+     :parent-id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu")
+   ( :headline " Thu, Feb 23                    this is a test!\n"
+     :id "d51e0c82-2005-4e7a-90d9-eb3024c4276a"
+     :group-id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu235fe0000369a4c2d171b16de8008479Calendar"
+     :parent-id "b92c491098343a3ee048af2ae7d5eebaBy client8c7bd4ee270a7b45899af20b5cb0f702Abraha, Seifu235fe0000369a4c2d171b16de8008479Calendar"))
+  (( :headline "* Calendar\n"
+     :id "4345f7664a902987ddd4372b5789b9bcCalendar"
+     :group-id "4345f7664a902987ddd4372b5789b9bc"
+     :parent-id nil)
+   ( :headline "** 2023\n"
+     :id "4345f7664a902987ddd4372b5789b9bcCalendar70fa3bddc1a414cff7cf6a31c3b481a32023"
+     :group-id "70fa3bddc1a414cff7cf6a31c3b481a3"
+     :parent-id "4345f7664a902987ddd4372b5789b9bcCalendar")
+   ( :headline "*** February\n"
+     :id "4345f7664a902987ddd4372b5789b9bcCalendar70fa3bddc1a414cff7cf6a31c3b481a32023ad56c1a9eb4e272501b3d49a683ea2a9February"
+     :group-id "ad56c1a9eb4e272501b3d49a683ea2a9"
+     :parent-id "4345f7664a902987ddd4372b5789b9bcCalendar70fa3bddc1a414cff7cf6a31c3b481a32023")
+   ( :headline "**** 23 Thursday    Abraha, Seifu       this is a test!\n"
+     :id "d51e0c82-2005-4e7a-90d9-eb3024c4276a"
+     :group-id "4345f7664a902987ddd4372b5789b9bcCalendar70fa3bddc1a414cff7cf6a31c3b481a32023ad56c1a9eb4e272501b3d49a683ea2a9February"
+     :parent-id "4345f7664a902987ddd4372b5789b9bcCalendar70fa3bddc1a414cff7cf6a31c3b481a32023ad56c1a9eb4e272501b3d49a683ea2a9February")))
+
+
+(setq yyy (-->
+	   (reorg--get-group-and-sort (list xxx) reorg--current-template 1 t)
+	   (reorg--get-all-tree-paths it (lambda (x)
+					   (and (listp x)
+						(stringp (car x))
+						(eq
+						 'leaf
+						 (get-text-property
+						  0
+						  'reorg-field-type
+						  (car x))))))
+	   ;; (reorg--walk-tree it (lambda (x)
+	   ;; 			  (let ((props (get-text-property 1 'reorg-data x)))
+	   ;; 			    (list :headline (org-no-properties (seq-copy x))
+	   ;; 				  :id (alist-get 'id props)
+	   ;; 				  :group-id (alist-get 'group-id props)
+	   ;; 				  :parent-id (alist-get 'parent-id props)))))
+	   ))
+
+(cl-loop for group in yyy
+	 collect (cl-loop with leaf = (car (last group))
+			  with headings = (reverse (butlast group))
+			  with y = nil
+			  for heading in headings
+			  for x from 0
+			  ;; try to find the lowest header possible 
+			  do (let ((props (get-text-property 1 'reorg-data heading)))
+			       (when (reorg--goto-next-prop 'id (alist-get 'id props))
+				 (setq y x)))
+			  finally (if (= y 0) (
+
+			  do (cl-loop for y from x downto 0
+				      do (reorg--insert-header-at-point (nth y headings)))
+			  
+
+
+
+
+
+
+
 
 
 
