15db7fb9d315c2235788f6086e3e08aec31eb5ee
Jeff Filipovits <jrfiliplovits@gmail.com>
2024-02-14 08:47:32 -0500

    wip

--- reorg-jeff.el
+++ reorg-jeff.el
@@ -127,11 +127,16 @@
 				       (car .ts-inactive-all)
 				       "") . reorg-string<)))))))
 
-(defun jrf/reorg-today-agenda ()
+
+(defun jrf/reorg-find-string-starting-with-substring (str list)
+  "Return the first string in LST that starts with the substring STR."
+  )
+
+(defun jrf/reorg-today-agenda (&optional date)
     (interactive)
     (reorg-org-capture-enable)
-    (let ((now (format-time-string "%Y-%m-%d"))
-	  (week-ago (ts-format "%Y-%m-%d" (ts-dec 'day 7 (ts-now)))))
+    (let* ((now (or date (format-time-string "%Y-%m-%d")))
+	   (week-ago (ts-format "%Y-%m-%d" (ts-dec 'day 7 (ts-parse now)))))
       (reorg-open-sidebar
        `( :sources ((org . ,reorg-test-org-file-list))
 	  :children
@@ -142,17 +147,33 @@
 					       "WAITING"
 					       "DEADLINE"))
 			       (not .archivedp))
-		      (propertize "Today" 'face '((t (:height 1.5)))))
+		      (propertize ,now
+				  'face '((t (:height 1.5)))
+				  'keymap (let ((map (make-sparse-keymap)))
+					    (define-key map (kbd "F")
+					      (lambda ()
+						(interactive)
+						(funcall
+						 #'jrf/reorg-today-agenda
+						 (ts-format "%Y-%m-%d"
+						 (ts-inc 'day 1 (ts-parse ,now))))))
+					    map)))
 	     :children (
-			( :group (when (reorg-org--ts-hhmm-p .ts-agenda-today)
+			( :group (when (cl-loop for each in .ts-active-flat
+						when (and (string-prefix-p ,now each)
+							  (reorg-org--ts-hhmm-p each))
+						return each)
 				   "--SCHEDULE--")
 			  :format-results
 			  (
 			   (s-pad-right 23  "."
 					(downcase
 					 (reorg-org--format-time-string 
-					  .ts-agenda-today
-					  "" "%-l:%M%p")))			 
+					  (cl-loop for each in '.ts-active-flat
+						when (and (string-prefix-p ,now each)
+							  (reorg-org--ts-hhmm-p each))
+						return each)
+					  "" "%-l:%M%p")))
 			   .headline
 			   " "
 			   (propertize (s-pad-right 18 " "
@@ -162,8 +183,10 @@
 			  :sort-results
 			  ((.ts-agenda-today . reorg-string<))
 			  )
-			( :group (when (and (or (and .ts-agenda-today
-						     (not (reorg-org--ts-hhmm-p .ts-agenda-today)))
+			( :group (when (and (or (cl-loop for each in .ts-active-flat
+							 when (and (string-prefix-p ,now each)
+							  (reorg-org--ts-hhmm-p each))
+							 return each)
 						(equal .priority "A"))
 					    (not .donep)
 					    (not .archivedp))
@@ -200,193 +223,193 @@
 			  :sort-results ((.root . reorg-string<)
 					 (.priority  . reorg-string<))))))))))
 
-(defun jrf/reorg-main ()
-  (interactive)
-  (reorg-org-capture-enable)
-  (let ((now (format-time-string "%Y-%m-%d"))
-	(week-ago (ts-format "%Y-%m-%d" (ts-dec 'day 7 (ts-now)))))
-    (reorg-open-sidebar
-     `( :sources ((org . ,reorg-test-org-file-list))
-	:format-results ( (s-pad-right 5 " " .priority)
-			  (s-pad-right 15 " " .todo)
-			  " "
-			  (if .ts-single
-			      (s-pad-right 50 "." .headline)
-			    .headline)
-			  (s-pad-right 35
-				       " "
-				       (if .ts-single
-					   (reorg-org--format-time-string
-					    .ts-single
-					    "%a, %b %d, %Y"
-					    "%a, %b %d, %Y at %-l:%M%p")
-					 ""))
-			  .clocked-time)
-	:children
-	(
-	 ( :group (when (and (member .todo '("TASK"
-					     "DELEGATED"
-					     "EVENT"
-					     "DONE"
-					     "OPP_DUE"
-					     "WAITING"
-					     "DEADLINE"))
-			     .ts-agenda-today)
-		    "Today")
-	   :format-results
-	   ((if (reorg-org--ts-hhmm-p .ts-agenda-today)
-		(s-pad-right 20 "."
-			     (downcase
-			      (reorg-org--format-time-string 
-			       .ts-agenda-today
-			       "" "%-l:%M%p")))
-	      (make-string 20 ? ))
-	    (s-pad-right 10 " " .todo)
-	    (s-pad-right 15 " " .category-inherited)
-	    (s-pad-right 50 " " .headline)
-	    (s-pad-left 30 " " .clocked-time))
-	   :sort-results
-	   ((.ts-agenda-today . string<)))	 
-	 ( :group "Cases"
-	   :children
-	   (( :group (when (and (or 
-				 (member .todo '("TASK"
-						 "DELEGATED"
-						 "EVENT"
-						 "OPP_DUE"
-						 "WAITING"
-						 "DEADLINE"))
-				 (string= .headline "_NOTES_"))
-				(not (member "prospective" .tag-list))
-				(not (member "closed" .tag-list))
-				(if .ts-single
-				    (org-string>= (reorg-org--format-time-string
-						   .ts-single
-						   "%Y-%m-%d")
-						  ,now)
-				  t))
-		       .root)
-	      :sort-groups reorg-string<
-	      :children (( :group (when (member .todo '("TASK"
-							"DELEGATED"
-							"WAITING"))
-				    "TASKS")
-			   :sort-groups (lambda (a b)
-					  (reorg--sort-by-list a b
-							       '("TASK"
-								 "DELEGATED"
-								 "WAITING")))
-			   :sort-results ((.priority . string<)
-					  (.todo . string<)
-					  (.headline . reorg-string<)))
-			 ( :group (when (and (or .ts-deadline
-						 .ts-active-first
-						 .ts-scheduled)
-					     ;; (org-string>= (reorg-org--format-time-string
-					     ;; 		      .ts-single
-					     ;; 		      "%Y-%m-%d")
-					     ;; 		     ,now)
-					     (not (member .todo '("TASK"
-								  "DONE"
-								  "DELEGATED"
-								  "WAITING"))))
-				    "CALENDAR")
-			   :sort-results ((.ts-single . string<)))
-			 ( :group (when (equal "_NOTES_" .headline) "")
-			   :format-results (.stars "  NOTES"))))))
+;; (defun jrf/reorg-main ()
+;;   (interactive)
+;;   (reorg-org-capture-enable)
+;;   (let ((now (format-time-string "%Y-%m-%d"))
+;; 	(week-ago (ts-format "%Y-%m-%d" (ts-dec 'day 7 (ts-now)))))
+;;     (reorg-open-sidebar
+;;      `( :sources ((org . ,reorg-test-org-file-list))
+;; 	:format-results ( (s-pad-right 5 " " .priority)
+;; 			  (s-pad-right 15 " " .todo)
+;; 			  " "
+;; 			  (if .ts-single
+;; 			      (s-pad-right 50 "." .headline)
+;; 			    .headline)
+;; 			  (s-pad-right 35
+;; 				       " "
+;; 				       (if .ts-single
+;; 					   (reorg-org--format-time-string
+;; 					    .ts-single
+;; 					    "%a, %b %d, %Y"
+;; 					    "%a, %b %d, %Y at %-l:%M%p")
+;; 					 ""))
+;; 			  .clocked-time)
+;; 	:children
+;; 	(
+;; 	 ( :group (when (and (member .todo '("TASK"
+;; 					     "DELEGATED"
+;; 					     "EVENT"
+;; 					     "DONE"
+;; 					     "OPP_DUE"
+;; 					     "WAITING"
+;; 					     "DEADLINE"))
+;; 			     .ts-agenda-today)
+;; 		    "Today")
+;; 	   :format-results
+;; 	   ((if (reorg-org--ts-hhmm-p .ts-agenda-today)
+;; 		(s-pad-right 20 "."
+;; 			     (downcase
+;; 			      (reorg-org--format-time-string 
+;; 			       .ts-agenda-today
+;; 			       "" "%-l:%M%p")))
+;; 	      (make-string 20 ? ))
+;; 	    (s-pad-right 10 " " .todo)
+;; 	    (s-pad-right 15 " " .category-inherited)
+;; 	    (s-pad-right 50 " " .headline)
+;; 	    (s-pad-left 30 " " .clocked-time))
+;; 	   :sort-results
+;; 	   ((.ts-agenda-today . string<)))	 
+;; 	 ( :group "Cases"
+;; 	   :children
+;; 	   (( :group (when (and (or 
+;; 				 (member .todo '("TASK"
+;; 						 "DELEGATED"
+;; 						 "EVENT"
+;; 						 "OPP_DUE"
+;; 						 "WAITING"
+;; 						 "DEADLINE"))
+;; 				 (string= .headline "_NOTES_"))
+;; 				(not (member "prospective" .tag-list))
+;; 				(not (member "closed" .tag-list))
+;; 				(if .ts-single
+;; 				    (org-string>= (reorg-org--format-time-string
+;; 						   .ts-single
+;; 						   "%Y-%m-%d")
+;; 						  ,now)
+;; 				  t))
+;; 		       .root)
+;; 	      :sort-groups reorg-string<
+;; 	      :children (( :group (when (member .todo '("TASK"
+;; 							"DELEGATED"
+;; 							"WAITING"))
+;; 				    "TASKS")
+;; 			   :sort-groups (lambda (a b)
+;; 					  (reorg--sort-by-list a b
+;; 							       '("TASK"
+;; 								 "DELEGATED"
+;; 								 "WAITING")))
+;; 			   :sort-results ((.priority . string<)
+;; 					  (.todo . string<)
+;; 					  (.headline . reorg-string<)))
+;; 			 ( :group (when (and (or .ts-deadline
+;; 						 .ts-active-first
+;; 						 .ts-scheduled)
+;; 					     ;; (org-string>= (reorg-org--format-time-string
+;; 					     ;; 		      .ts-single
+;; 					     ;; 		      "%Y-%m-%d")
+;; 					     ;; 		     ,now)
+;; 					     (not (member .todo '("TASK"
+;; 								  "DONE"
+;; 								  "DELEGATED"
+;; 								  "WAITING"))))
+;; 				    "CALENDAR")
+;; 			   :sort-results ((.ts-single . string<)))
+;; 			 ( :group (when (equal "_NOTES_" .headline) "")
+;; 			   :format-results (.stars "  NOTES"))))))
 	 
-	 ( :group "Prospective cases"
-	   :children (( :group (when (and (or 
-					   (member .todo '("TASK"
-							   "DELEGATED"
-							   "EVENT"
-							   "OPP_DUE"
-							   "WAITING"
-							   "DEADLINE"))
-					   ;; .ts-single)
+;; 	 ( :group "Prospective cases"
+;; 	   :children (( :group (when (and (or 
+;; 					   (member .todo '("TASK"
+;; 							   "DELEGATED"
+;; 							   "EVENT"
+;; 							   "OPP_DUE"
+;; 							   "WAITING"
+;; 							   "DEADLINE"))
+;; 					   ;; .ts-single)
 
-					   ;; (and .ts-single
-					   ;;      (org-string>= (reorg-org--format-time-string
-					   ;; 		      .ts-single
-					   ;; 		      "%Y-%m-%d")
-					   ;; 		     ,now))
-					   (string= .headline "_NOTES_"))
-					  (member "prospective" .tag-list))
-				 .root)
-			:sort-groups reorg-string<
-			:children (( :group (when (member .todo '("TASK"
-								  "DELEGATED"
-								  "WAITING"))
-					      "TASKS")
-				     :sort-groups (lambda (a b)
-						    (reorg--sort-by-list a b
-									 '("TASK"
-									   "DELEGATED"
-									   "WAITING")))
-				     :sort-results ((.priority . string<)
-						    (.todo . string<)
-						    (.headline . reorg-string<)))
-				   ( :group (when (and (or .ts-deadline
-							   .ts-active-first
-							   .ts-scheduled)
+;; 					   ;; (and .ts-single
+;; 					   ;;      (org-string>= (reorg-org--format-time-string
+;; 					   ;; 		      .ts-single
+;; 					   ;; 		      "%Y-%m-%d")
+;; 					   ;; 		     ,now))
+;; 					   (string= .headline "_NOTES_"))
+;; 					  (member "prospective" .tag-list))
+;; 				 .root)
+;; 			:sort-groups reorg-string<
+;; 			:children (( :group (when (member .todo '("TASK"
+;; 								  "DELEGATED"
+;; 								  "WAITING"))
+;; 					      "TASKS")
+;; 				     :sort-groups (lambda (a b)
+;; 						    (reorg--sort-by-list a b
+;; 									 '("TASK"
+;; 									   "DELEGATED"
+;; 									   "WAITING")))
+;; 				     :sort-results ((.priority . string<)
+;; 						    (.todo . string<)
+;; 						    (.headline . reorg-string<)))
+;; 				   ( :group (when (and (or .ts-deadline
+;; 							   .ts-active-first
+;; 							   .ts-scheduled)
 
-						       ;; (org-string>= (reorg-org--format-time-string
-						       ;; 		      .ts-single
-						       ;; 		      "%Y-%m-%d")
-						       ;; 		     ,now)
-						       (not (member .todo '("TASK"
-									    "DONE"
-									    "DELEGATED"
-									    "WAITING"))))
-					      "CALENDAR")
-				     :sort-results ((.ts-single . string<)))
-				   ( :group (when (equal "_NOTES_" .headline) "")
-				     :format-results (.stars "  NOTES"))))))
-	 ( :group (when (and (member .todo '("TASK"
-					     "DELEGATED"
-					     "WAITING"))
-			     .property.delegated)
-		    "Delegations")
-	   :format-results ((s-pad-right 5 " " .priority)
-			    (s-pad-right 10 " " .todo)
-			    (s-pad-right 20 " " .category-inherited)
-			    " "
-			    .headline)
-	   :children (( :group .delegated
-			:sort-results ((.category-inherited . reorg-string<)
-				       (.priority . reorg-string<)
-				       (.headline . reorg-string<))
-			:sort-groups reorg-string<)))
-	 ( :group (when (and (member .todo '("TASK"
-					     "DELEGATED"
-					     ))
-			     (if .ts-single
-				 (org-string>= (reorg-org--format-time-string
-						.ts-single
-						"%Y-%m-%d")
-					       ,now)
-			       t))
-		    "Priorities")
-	   :sort-results ((.category-inherited . reorg-string<)
-			  (.todo . (lambda (a b)
-				     (reorg--sort-by-list a
-							  b
-							  '("TASK"
-							    "DELEGATED"
-							    "WAITING")))))
-	   :format-results ((s-pad-right 10 " " .todo)
-			    (s-pad-right 15 " " .category-inherited)
-			    (if .ts-single
-				(s-pad-right 50 "." .headline)
-			      .headline)
-			    (when .ts-single
-			      (reorg-org--format-time-string
-			       .ts-single
-			       "%a, %b %d, %Y"
-			       "%a, %b %d, %Y at %-l:%M%p")))
-	   :children (( :group (when (equal .priority "A") "High"))
-		      ( :group (when (equal .priority "B") "Medium"))
-		      ( :group (when (equal .priority "C") "Low")))))))))
+;; 						       ;; (org-string>= (reorg-org--format-time-string
+;; 						       ;; 		      .ts-single
+;; 						       ;; 		      "%Y-%m-%d")
+;; 						       ;; 		     ,now)
+;; 						       (not (member .todo '("TASK"
+;; 									    "DONE"
+;; 									    "DELEGATED"
+;; 									    "WAITING"))))
+;; 					      "CALENDAR")
+;; 				     :sort-results ((.ts-single . string<)))
+;; 				   ( :group (when (equal "_NOTES_" .headline) "")
+;; 				     :format-results (.stars "  NOTES"))))))
+;; 	 ( :group (when (and (member .todo '("TASK"
+;; 					     "DELEGATED"
+;; 					     "WAITING"))
+;; 			     .property.delegated)
+;; 		    "Delegations")
+;; 	   :format-results ((s-pad-right 5 " " .priority)
+;; 			    (s-pad-right 10 " " .todo)
+;; 			    (s-pad-right 20 " " .category-inherited)
+;; 			    " "
+;; 			    .headline)
+;; 	   :children (( :group .delegated
+;; 			:sort-results ((.category-inherited . reorg-string<)
+;; 				       (.priority . reorg-string<)
+;; 				       (.headline . reorg-string<))
+;; 			:sort-groups reorg-string<)))
+;; 	 ( :group (when (and (member .todo '("TASK"
+;; 					     "DELEGATED"
+;; 					     ))
+;; 			     (if .ts-single
+;; 				 (org-string>= (reorg-org--format-time-string
+;; 						.ts-single
+;; 						"%Y-%m-%d")
+;; 					       ,now)
+;; 			       t))
+;; 		    "Priorities")
+;; 	   :sort-results ((.category-inherited . reorg-string<)
+;; 			  (.todo . (lambda (a b)
+;; 				     (reorg--sort-by-list a
+;; 							  b
+;; 							  '("TASK"
+;; 							    "DELEGATED"
+;; 							    "WAITING")))))
+;; 	   :format-results ((s-pad-right 10 " " .todo)
+;; 			    (s-pad-right 15 " " .category-inherited)
+;; 			    (if .ts-single
+;; 				(s-pad-right 50 "." .headline)
+;; 			      .headline)
+;; 			    (when .ts-single
+;; 			      (reorg-org--format-time-string
+;; 			       .ts-single
+;; 			       "%a, %b %d, %Y"
+;; 			       "%a, %b %d, %Y at %-l:%M%p")))
+;; 	   :children (( :group (when (equal .priority "A") "High"))
+;; 		      ( :group (when (equal .priority "B") "Medium"))
+;; 		      ( :group (when (equal .priority "C") "Low")))))))))
 
 (defun jrf/reorg-billing-log ()
   ""
